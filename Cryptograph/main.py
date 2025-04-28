a29f9640b106257a66f67117593f9c3b7d024142
setuser1 <ss37138@sersd.org>
2025-04-28 14:19:44 -0400

    Add files via upload

---
+++ Cryptograph/main.py
@@ -0,0 +1,25 @@
+import crypto
+import random
+
+mixkey = crypto.main()  # Assuming this returns an integer
+
+
+
+characters = [
+        # Uppercase letters
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        
+        # Lowercase letters
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        
+        # Digits
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        
+        # Basic specials
+        '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+        ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',
+]
+
+print(mixkey)
