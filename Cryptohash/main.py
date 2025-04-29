be0c9cb824cfbea304dc0ed53143a4a288ec3f4d
setuser1 <ss37138@sersd.org>
2025-04-29 08:37:18 -0400

    Update main.py

--- Cryptohash/main.py
+++ Cryptohash/main.py
@@ -1,1 +1,11 @@
+import algo
 
+send = str(input("Enter msg: "))
+
+# recieve = str(input("Enter recieved msg: ")) # uncomment if needed
+
+send = algo.encode(send)
+print(send)
+
+# recieve = algo.decode(recieve) # uncomment if needed
+# print(recieve)
