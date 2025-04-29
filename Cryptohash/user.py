525ee1491e736b811327ed43f535dd1725bac308
setuser1 <ss37138@sersd.org>
2025-04-29 08:32:58 -0400

    Update user.py

--- Cryptohash/user.py
+++ Cryptohash/user.py
@@ -3,20 +3,15 @@ import random
 def main():
     constant = 395  # Example prime number, should be much larger in real scenarios
     pubkey = random.randint(500, 1000)
-    privkey1 = random.randint(500, 1000)
-    privkey2 = random.randint(500, 1000)
+    privkey = random.randint(500, 1000)
 
-# Both participants compute their key messages using the public key and their private keys
+    keymsg1 = pubkey*privkey1*constant # send this to other participent
 
-    keymsg1 = pubkey*privkey1*constant
-    keymsg2 = pubkey*privkey2*constant
+    othermsg = int(input("Enter key msg from other.")
+    complete = othermsg*privkey*constant
 
-# Now they exchange and compute the secret
-    complete1 = keymsg2*privkey1*constant  # Participant 1 computes complete secret
-    complete2 = keymsg1*privkey2*constant  # Participant 2 computes complete secret
-
-    secret = complete1
+    secret = complete
 # If both computed secrets are the same, the key exchange is successful
-    return secret if complete1 == complete2 else None
+    return secret
 
 mixkey = main()
