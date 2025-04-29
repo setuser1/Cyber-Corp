e2890c0501b752d09889aac8b666215c9292370a
setuser1 <ss37138@sersd.org>
2025-04-29 09:47:12 -0400

    Update user.py

--- Cryptohash/user.py
+++ Cryptohash/user.py
@@ -3,7 +3,7 @@ import random
 def main():
     # Shared values (agreed ahead of time)
     prime = 397  # Insecurely small, just for demo
-    base = random.randint(2, prime - 2)
+    base = 30
 
     # Your private key and public key
     privkey = random.randint(500, 1000)
