f6218cb55e738c4eae8da61fa66cc88b3eb5649c
setuser1 <ss37138@sersd.org>
2025-04-30 20:11:34 -0400

    Update socket_test.py

--- encryption/socket_test.py
+++ encryption/socket_test.py
@@ -49,7 +49,7 @@ def server_mode():
     print(f"Public key sent: {pubkey}")
 
     # Receive the client's public key
-    other_pubkey = int(addr.recv(1024).decode())
+    other_pubkey = int(s.recv(1024).decode())
     print(f"Public key received: {other_pubkey}")
 
     # Perform key exchange
