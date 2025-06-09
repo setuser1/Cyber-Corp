5dddfdd69eb5bc66cdb46f4eaf167331f22cd062
setuser1 <ss37138@sersd.org>
2025-06-09 12:24:19 -0400

    Update printexample.c

--- C-Lang/printexample.c
+++ C-Lang/printexample.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+int main() {
+    char *string = malloc(101);
+    strcpy(string, "Hello World!");
+    write(STDOUT_FILENO, string, strlen(string));
+    free(string);
+    return 0;
+}
