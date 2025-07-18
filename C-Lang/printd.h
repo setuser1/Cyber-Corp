50d936f9356a84b5bab4a70064abb25375438cc1
setuser1 <ss37138@sersd.org>
2025-07-17 21:07:25 -0400

    Update printd.h

--- C-Lang/printd.h
+++ C-Lang/printd.h
@@ -3,15 +3,14 @@
 
 #include <stdarg.h>
 
-// Your custom printf-like function (supports %d, %s, %c, %%)
+// No more ai comments
 void printd(const char *buff, ...);
 
-// Helper functions used internally or externally
 void print_int(int i);
 void print_string(const char *s);
 void print_c(const char *s);
-void print_float(double val);  // Optional if you plan to finish this
+void print_float(double val);
 void print_long_long(long long i);
 
 
-#endif // PRINTD_H
+#endif
