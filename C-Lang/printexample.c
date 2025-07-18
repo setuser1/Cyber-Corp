0b1781107b0f514fd4eab50ee4466aca3aef4c39
setuser1 <ss37138@sersd.org>
2025-07-17 21:09:42 -0400

    Update printexample.c

--- C-Lang/printexample.c
+++ C-Lang/printexample.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "printd.h"
 
 int main() {
+    // the pro way of printing
     char *string = malloc(101);
     strcpy(string, "Hello World!");
     write(STDOUT_FILENO, string, strlen(string));
     free(string);
 
     // the noob way of printing
-    printf("Hello World!);
-    
+    printf("Hello World!");
+
+    // the hacker way of printing    
+    printd("Hello World!");
     return 0;
 }
