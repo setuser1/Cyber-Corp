55ffa5cc3b0d5acad8179fbdd98e5039f62812d1
setuser1 <ss37138@sersd.org>
2025-07-26 15:47:56 -0400

    Update printd.h

--- C-Lang/printd.h
+++ C-Lang/printd.h
@@ -1,16 +1,46 @@
 #ifndef PRINTD_H
 #define PRINTD_H
+#include <stdarg.h> // useless in clion
 
-#include <stdarg.h>
-
-// No more ai comments
-void printd(const char *buff, ...);
-
-void print_int(int i);
-void print_string(const char *s);
-void print_c(const char *s);
-void print_float(double val);
-void print_long_long(long long i);
-
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+    /**
+     * @brief Custom version of printf by yours truly. It lacks many of the features.
+     * @param format The format.
+     * @param ...    Additional arguments depending on format specifiers.
+     */
+    void printd(const char *format, ...);
+
+    /**
+     * @brief Prints an integer.
+     */
+    void print_int(int value);
+
+    /**
+     * @brief Prints a string.
+     */
+    void print_string(const char *str);
+
+    /**
+     * @brief Print a single character.
+     *        Is equal to the string func but checks for length to be 1.
+     */
+    void print_c(const char *str);
+
+    /**
+     * @brief Prints a double type along with a float type if needed.
+     */
+    void print_float(double value);
+
+    /**
+     * @brief Print a long-long integer.
+     */
+    void print_long_long(long long value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif PRINTD_H
